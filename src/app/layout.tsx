05ce99a43af4db66c1981c20d787bea2a6023984
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2025-06-18 18:28:49 +0700

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,34 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import SessionWrapper from "@/app/_components/auth/SessionProvider";
-import { Toaster } from "sonner";
-import ReactQueryProvider from "@/app/_components/utils/TanstackQueryProvider";
+import type { Metadata } from 'next';
+import './globals.css';
+import SessionWrapper from '@/app/_components/auth/SessionProvider';
+import { Toaster } from 'sonner';
+import ReactQueryProvider from '@/app/_components/utils/TanstackQueryProvider';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
-  title: "Focus Learning Bali",
-  description: "",
-  icons: "./assets/icon.svg",
+	title: 'Focus Learning Bali',
+	description: '',
+	icons: './assets/icon.svg',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <ReactQueryProvider>
-      <SessionWrapper>
-        <html lang="en">
-          <body>
-            {children}
-            <Toaster />
-          </body>
-        </html>
-      </SessionWrapper>
-    </ReactQueryProvider>
-  );
+	return (
+		<ReactQueryProvider>
+			<SessionWrapper>
+				<html lang="en">
+					<body>
+						{children}
+						<Toaster />
+						<Analytics />
+						<SpeedInsights />
+					</body>
+				</html>
+			</SessionWrapper>
+		</ReactQueryProvider>
+	);
 }
